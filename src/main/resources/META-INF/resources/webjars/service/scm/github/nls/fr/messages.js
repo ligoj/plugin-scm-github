bdbccd6f9b7e36c85ef58ea29b15a3c47b882b30
Fabrice Daugan <fabrice.daugan@gfi.fr>
2017-09-11 15:29:27 +0200

    Initial version

---
+++ src/main/resources/META-INF/resources/webjars/service/scm/github/nls/fr/messages.js
@@ -0,0 +1,10 @@
+define({
+	'service:scm:github:url': 'URL de base',
+	'service:scm:github:repository': 'Dépôt',
+	'service:scm:github:user': 'Utilisateur',
+	'service:scm:github:password': 'Mot de passe',
+	'error': {
+		'github-admin': 'Accès administrateur échoué',
+		'github-repository': 'Dépôt Subversion invalide'
+	}
+});
---
+++ src/main/resources/META-INF/resources/webjars/service/scm/github/nls/messages.js
@@ -0,0 +1,14 @@
+define({
+	root: {
+		'service:scm:github:url': 'Base URL',
+		'service:scm:github': 'GitHub',
+		'service:scm:svn:repository': 'Repository',
+		'service:scm:svn:user': 'User',
+		'service:scm:svn:password': 'Password',
+		'error': {
+			'github-admin': 'Administrator access failed',
+			'github-repository': 'Invalid Github repository'
+		}
+	},
+	fr: true
+});
